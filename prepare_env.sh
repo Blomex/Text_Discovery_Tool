2426a428cec0033507959e1f650b9f8aff632f91
Blomex <blomex.bloomex@gmail.com>
2021-05-26 00:05:35 +0200

    includes

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,6 +1,7 @@
-echo GOOGLE_CLIENT_ID: ${GOOGLE_CLIENT_ID} >> .env.yaml
-echo GOOGLE_CLIENT_SECRET: ${GOOGLE_CLIENT_SECRET} >> .env.yaml
-echo UPLOAD_BUCKET: ${UPLOAD_BUCKET} >> .env.yaml
-echo PROJECT_ID: ${PROJECT_ID} >> .env.yaml
-echo SECOND_BUCKET: ${SECOND_BUCKET} >> .env.yaml
-echo SENDGRID_API_KEY: ${SENDGRID_API_KEY} >> .env.yaml
+echo env_variables: >>.env.yaml
+echo "  GOOGLE_CLIENT_ID: ${GOOGLE_CLIENT_ID}" >> .env.yaml
+echo  " GOOGLE_CLIENT_SECRET: ${GOOGLE_CLIENT_SECRET}" >> .env.yaml
+echo "  UPLOAD_BUCKET: ${UPLOAD_BUCKET}" >> .env.yaml
+echo "  PROJECT_ID: ${PROJECT_ID}" >> .env.yaml
+echo "  SECOND_BUCKET: ${SECOND_BUCKET}" >> .env.yaml
+echo "  SENDGRID_API_KEY: ${SENDGRID_API_KEY}" >> .env.yaml
